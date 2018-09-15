fced344ae617c4d050c1634b7bd935c61a7fa367
Jon Cederqvist <jon.cederqvist@direktchark.se>
2018-09-15 23:46:35 +0200

    added replacement project

---
+++ src/Modbus.Protocol/TcpTransport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ModbusOld = Modbus.Net;
+namespace Modbus.Protocol
+{
+    class TcpTransport : Modbus.Net.TcpConnector
+    {
+        public TcpTransport(string ipaddress, int port, int timeoutTime) : base(ipaddress, port, timeoutTime)
+        {
+        }
+    }
+}
---
+++ src/Modbus.Simple/ModbusMachine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modbus.Simple
+{
+    class ModbusMachine
+    {
+        public object Node { get; set; }
+        public Object[] Addresses;
+    }
+}
